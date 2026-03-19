467d714a8c07760fa5ef10ab2e6340909a81944a
EribyteVT <142944705+EribyteVT@users.noreply.github.com>
2026-03-19 14:37:08 -0500

    Add files via upload

---
+++ talents.js
@@ -0,0 +1,178 @@
+const talents = [
+  {
+    name: "EribyteVT",
+    tagline: "Software Engineer",
+    image: "03.png",
+    accent: "#00ffff",
+    twitchChannel: "EribyteVT",
+    bio: `Eribyte is your Average Software Engineer, residing more on the backend of the spectrum she likes SQL, Java, and Especially Python. 
+          She's often found streaming whatever she wants on a given day, usually something either niche or at least a decade old .
+          She likes Editing, Rootbeer, Monster Energy, and her home state of MINNESOTA BABY SKI-U-MAH GO VIKINGS RAAAAAAAAAAAAAAAAAAAAAAAAAAH
+          `,
+    youtubeHandle: "@EribyteVT",
+    socials: [
+      {
+        url: "https://www.twitch.tv/EribyteVT",
+        icon: "fa-brands fa-twitch",
+        label: "Twitch",
+      },
+      {
+        url: "https://www.youtube.com/@EribyteVT",
+        icon: "fa-brands fa-youtube",
+        label: "YouTube",
+      },
+      {
+        url: "https://www.tiktok.com/@EribyteVT",
+        icon: "fa-brands fa-tiktok",
+        label: "Tiktok",
+      },
+      {
+        url: "https://www.discord.gg/8pM8kFvP5C",
+        icon: "fa-brands fa-discord",
+        label: "Discord",
+      },
+      {
+        url: "https://x.com/EribyteVT",
+        icon: "fa-brands fa-x-twitter",
+        label: "Twitter",
+      },
+    ],
+  },
+  {
+    name: "DragonCatsVT",
+    tagline: "Vampire Bartender",
+    image: "wokeesex.png",
+    accent: "#00ffa2",
+    twitchChannel: "dragoncatsvt",
+    bio: `Valerie von Weisse is a Vampire over 500 years old, although nowadays she's a bartender, not because she needs the money of course hahaha what a silly thing to think
+    She can be found streaming a variety of different games, usually those suggested by chat in her discord
+    she enjoys Transformers, Castlevania, SIGNALIS, and of course women of all kinds`,
+    youtubeHandle: "@DragonCatsVT",
+    socials: [
+      {
+        url: "https://www.twitch.tv/dragoncatsvt",
+        icon: "fa-brands fa-twitch",
+        label: "Twitch",
+      },
+      {
+        url: "https://discord.com/invite/FYEabfMVD7",
+        icon: "fa-brands fa-discord",
+        label: "Discord",
+      },
+      {
+        url: "https://www.youtube.com/@DragonCatsVT",
+        icon: "fa-brands fa-youtube",
+        label: "YouTube",
+      },
+      {
+        url: "https://www.youtube.com/@DragonCatsVTVODS",
+        icon: "fa-brands fa-youtube",
+        label: "VODs",
+      },
+      {
+        url: "https://x.com/DragonCatsVT",
+        icon: "fa-brands fa-x-twitter",
+        label: "Twitter",
+      },
+    ],
+  },
+  {
+    name: "MinaMoth",
+    tagline: "Goofy Mothgirl!",
+    image: "./minami.png",
+    accent: "#ff6600",
+    twitchChannel: "minamothvt",
+    bio: `Mina is a moth, specifically a Cecropia Moth. When not pondering lamps they're often found being goofy somewhere on the internet
+          You can usually find them streaming hero shooters with her friends or just talking to chat
+          She enjoys moths, Tarot, Pokemon Cards, AND THE BIG LAMP IN THE SKY, ONE DAY SHE WILL REACH IT`,
+    youtubeHandle: "@MinaMothVT",
+    socials: [
+      {
+        url: "https://www.twitch.tv/minamothvt",
+        icon: "fa-brands fa-twitch",
+        label: "Twitch",
+      },
+      {
+        url: "https://www.youtube.com/@MinaMothVT",
+        icon: "fa-brands fa-youtube",
+        label: "YouTube",
+      },
+      {
+        url: "https://www.tiktok.com/@minamothvt",
+        icon: "fa-brands fa-tiktok",
+        label: "TikTok",
+      },
+      {
+        url: "https://discord.gg/jPKpUDyKKM",
+        icon: "fa-brands fa-discord",
+        label: "Discord",
+      },
+    ],
+  },
+  {
+    name: "InternetRain",
+    tagline: "hacker trapped inside your PC",
+    image: "Netto_fugi.png",
+    accent: "#00fcd5",
+    twitchChannel: "internetrain",
+    bio: `Netto is a hacker inside of your PC at this very moment, stealing your CPU power to make her Breakcore music (that you can listen to on spotify and bandcamp btw)
+          When not making her sick jams she's often found streaming retro style indie games and shitposting on the internet
+          Netto likes Breakcore, Brainrot, making music, and gunk (Goth + Punk) styles`,
+    youtubeHandle: "@internetrain",
+    socials: [
+      {
+        url: "https://www.twitch.tv/internetrain",
+        icon: "fa-brands fa-twitch",
+        label: "Twitch",
+      },
+      {
+        url: "https://youtube.com/@internetrain?si=tHnkIHQCruz9U-Gr&sub_confirmation=1",
+        icon: "fa-brands fa-youtube",
+        label: "YouTube",
+      },
+      {
+        url: "https://discord.gg/TC7NyGsWtM",
+        icon: "fa-brands fa-discord",
+        label: "Discord",
+      },
+      {
+        url: "http://internetrain.bandcamp.com/",
+        icon: "fa-brands fa-bandcamp",
+        label: "Bandcamp",
+      },
+      {
+        url: "https://open.spotify.com/artist/7hCQp6KXZw6xeW3U8JFI6D?utm_medium=share&utm_source=linktree",
+        icon: "fa-brands fa-spotify",
+        label: "Spotify",
+      },
+    ],
+  },
+  {
+    name: "PandoraJusticeDev",
+    tagline: "Game Developer and Cozy Girl",
+    image: "pandorka.png",
+    accent: "#ffff00",
+    twitchChannel: "pandorajusticedev",
+    bio: `Pandora is a game developer that has a game on steam as well as a few game jams under her belt
+          She can often be found streaming the earth defense force series, and occasionally Super Mario 64 and teaching others how to speedrun it
+          Pandora enjoys Tahitian treat, Game development, Table Top RPG's, and Dragonball Z amv's`,
+    youtubeHandle: "@PandoraJustice",
+    socials: [
+      {
+        url: "https://www.twitch.tv/pandorajusticedev",
+        icon: "fa-brands fa-twitch",
+        label: "Twitch",
+      },
+      {
+        url: "https://www.youtube.com/@PandoraJustice",
+        icon: "fa-brands fa-youtube",
+        label: "YouTube",
+      },
+      {
+        url: "https://bsky.app/profile/pandorajusticedev.bsky.social",
+        icon: "fa-brands fa-bluesky",
+        label: "Bluesky",
+      },
+    ],
+  },
+];
